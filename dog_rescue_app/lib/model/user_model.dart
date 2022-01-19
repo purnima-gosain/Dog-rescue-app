0b658d6d24b6f5b2b0b7b2bc5830e8fd93056483
purnima-poo <np03a190173@herladcollege.edu.np>
2022-01-19 22:40:25 +0545

    linked the server

---
+++ dog_rescue_app/lib/model/user_model.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserModel {
+  String? uid;
+  String? email;
+  String? firstName;
+  String? secondName;
+
+  UserModel({this.uid, this.email, this.firstName, this.secondName});
+
+  //taking data from the server
+  factory UserModel.fromMap(map) {
+    return UserModel(
+      uid: map['uid'],
+      email: map['email'],
+      firstName: map['firstName'],
+      secondName: map['secondName'],
+    );
+  }
+
+  //sending data to our server
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'firstName': firstName,
+      'secondName': secondName,
+    };
+  }
+}
