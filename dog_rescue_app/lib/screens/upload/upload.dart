1530b56236bab4f55bb87825c17d2b31d1c9c3cc
purnima-poo <np03a190173@herladcollege.edu.np>
2022-02-04 10:48:40 +0545

    added age controller

--- dog_rescue_app/lib/screens/upload/upload.dart
+++ dog_rescue_app/lib/screens/upload/upload.dart
@@ -14,16 +14,29 @@ class _UploadScreenState extends State<UploadScreen> {
 
   @override
   Widget build(BuildContext context) {
+    //description field
     final descrptionField = TextFormField(
-        autofocus: false,
-        controller: descriptionEditingController,
-        keyboardType: TextInputType.multiline,
-        maxLines: null,
-        validator: (value) {
-          if (value!.isEmpty) {
-            return "Please write the description";
-          }
-        });
+      autofocus: false,
+      controller: descriptionEditingController,
+      keyboardType: TextInputType.multiline,
+      maxLines: null,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "Please write the description";
+        }
+      },
+      onSaved: (value) {
+        descriptionEditingController.text = value!;
+      },
+    );
+//agefield
+    final ageField = TextFormField(
+      autofocus: false,
+      controller: ageEditingController,
+      keyboardType: TextInputType.number,
+      maxLength: 100,
+    );
+
     return Container();
   }
 }
